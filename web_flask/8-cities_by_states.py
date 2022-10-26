e6947230da980a6552cc722bbdcfc91562d5f0f1
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2022-10-26 13:21:42 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from models.city import City
-
-
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/cities_by_states', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html',
-                           Table="States",
-                           states=states)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

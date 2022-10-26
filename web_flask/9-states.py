59f83f3ab1ed17c6217f6cdafea1b45d6500d15f
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2022-10-26 13:22:27 +0100

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from models.city import City
-
-
 app = Flask(__name__)
 
 
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/states/', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def display_html(id=None):
-    """ Function called with /states route """
-    states = storage.all(State)
-
-    if not id:
-        dict_to_html = {value.id: value.name for value in states.values()}
-        return render_template('7-states_list.html',
-                               Table="States",
-                               items=dict_to_html)
-
-    k = "State.{}".format(id)
-    if k in states:
-        return render_template('9-states.html',
-                               Table="State: {}".format(states[k].name),
-                               items=states[k])
-
-    return render_template('9-states.html',
-                           items=None)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

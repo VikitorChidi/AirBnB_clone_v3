75b60705ffc27e75a06140f37678dc6bc304a297
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2022-10-26 13:13:49 +0100

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""
+starts a Flask web application
+"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Function called with / route """
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Function called with /hbnb route """
+    """returns HBNB"""
     return 'HBNB'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
